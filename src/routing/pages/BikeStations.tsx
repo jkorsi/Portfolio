357a8a29e7d29eb431a9caa019242366961cb225
Juho Suni <juho.suni@protonmail.com>
2023-05-29 12:54:14 +0300

    Renamed stations table

---
+++ src/routing/pages/BikeStations.tsx
@@ -0,0 +1,13 @@
+import { StationsTable } from "../../components/StationsTable";
+
+const BikeStations = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center">
+        <StationsTable />
+      </div>
+    </div>
+  );
+};
+
+export default BikeStations;
